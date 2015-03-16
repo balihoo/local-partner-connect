fd835a43a0f725107d3ebc0b639c5a2645fefd16
Jordan Lutz <jlutz@balihoo.com>
2015-03-16 15:14:41 -0600

    Added location metric data to view

--- app/scripts/controllers/localWebsite.js
+++ app/scripts/controllers/localWebsite.js
@@ -8,12 +8,7 @@
  * Controller of the locationPluginApp
  */
 angular.module('locationPluginApp')
-  .controller('LocalWebsiteController', function ($scope, $rootScope, $timeout, AUTH_EVENTS, AuthService) {
-
-    //$q.when(connection.getWebsiteMetrics())
-    //  .then(function (websiteMetrics) {
-    //    console.log(websiteMetrics);
-    //  });
+  .controller('LocalWebsiteController', function ($scope, $rootScope, $q, $window, $timeout, AUTH_EVENTS, AuthService) {
 
     if (AuthService.isAuthenticated()) {
       getLocalWebsiteData();
@@ -28,7 +23,22 @@ angular.module('locationPluginApp')
     }
 
     function getLocalWebsiteData() {
-      console.log('This is a test!');
+      $q.when($scope.connection.getWebsiteMetrics())
+        .then(function (websiteMetrics) {
+          if (!websiteMetrics) {
+            $scope.toggleLocalWebsiteModal();
+            $('#localWebsiteModal').on('hidden.bs.modal', function (){
+              $window.location.reload();
+            });
+            throw new Error("No local website data was found");
+          }
+          return $scope.websiteMetrics = websiteMetrics;
+        })
+        .catch(function(response) {
+          if (response.status == 404) {
+            $window.location.reload();
+          }
+        });
     }
 
   });
