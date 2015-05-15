843e3dbbf08fd0f9e5b4d10f593e43d01de10262
Jordan Lutz <jlutz@balihoo.com>
2015-05-15 09:48:28 -0600

    Added custom attribute directive for tooltips; added tooltips content to tactic metrics

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -205,6 +205,18 @@ angular
       }
     };
   })
+  .directive('tooltip', function() {
+    return {
+      restrict: 'A',
+      link: function(scope, element, attrs) {
+        $(element).hover(function() {
+          $(element).tooltip('show');
+        }, function() {
+          $(element).tooltip('hide');
+        });
+      }
+    };
+  })
   .constant('AUTH_EVENTS', {
     loginSuccess: 'auth-login-success',
     loginFailed: 'auth-login-failed',
