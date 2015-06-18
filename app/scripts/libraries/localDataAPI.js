a0f45eb44a8fc8effd20d76f0fd38a5aa0021d63
Jordan Lutz <jlutz@balihoo.com>
2015-06-18 10:07:37 -0600

    Updated endpoints to use Balihoo dev environment

--- app/scripts/libraries/localDataAPI.js
+++ app/scripts/libraries/localDataAPI.js
@@ -2,7 +2,7 @@
   exports.balihoo = exports.balihoo || {};
 
   var version = "v1.0";
-  var defaultUrl = "http://bac.balihoo-cloud.com";
+  var defaultUrl = "http://bac.dev.balihoo-cloud.com";
 
   /**
    * constructor for a LocationApi object
