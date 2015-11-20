ab06e2d8057fba80af6c0896d3883e188f757c18
Jordan Lutz <jlutz@balihoo.com>
2015-11-20 12:15:34 -0700

    Fix UI issue on Profile tab; add new LPC API endpoints

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -121,7 +121,7 @@ angular
       this.config = {};
       this.clientId = clientId;
       this.clientApiKey = clientApiKey;
-      this.config.baseUrl = 'https://bac.dev.balihoo-cloud.com';  // Dev environment LPC API
+      this.config.baseUrl = 'https://balihoo-bac-dev.elasticbeanstalk.com';  // Dev environment LPC API
       this.connection = new balihoo.LocalConnection(clientId, clientApiKey, this.config);
     };
     this.destroy = function () {
