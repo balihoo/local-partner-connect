b42304bc0d0539e10e4554815f53f77917e18e24
Jordan Lutz <jlutz@balihoo.com>
2016-04-06 16:30:06 -0600

    Fix menu navigation bug

--- app/scripts/controllers/campaigns.js
+++ app/scripts/controllers/campaigns.js
@@ -13,11 +13,12 @@ angular.module('locationPluginApp')
     '$rootScope',
     '$window',
     '$http',
+    '$location',
     '$q',
     '$timeout',
     'AuthService',
 
-    function ($scope, $rootScope, $window, $http, $q, $timeout, AuthService) {
+    function ($scope, $rootScope, $window, $http, $location, $q, $timeout, AuthService) {
 
       $timeout(function() {
         if (AuthService.isAuthenticated()) {
@@ -34,13 +35,19 @@ angular.module('locationPluginApp')
         .then(function (websiteMetrics) {
           if (websiteMetrics.visits.total + websiteMetrics.leads.total > 0)
             $scope.menu[1].show = true;
+            if ($scope.menu[0].show == false && $scope.menu[1].show == true) {
+                $location.path('/localWebsite')
+            }
         });
 
       $q.when($scope.connection.getProfileForm())
         .then(function (profileForm) {
           if (profileForm)
             $scope.menu[2].show = true;
-        })
+          if ($scope.menu[0].show == false && $scope.menu[1].show == false && $scope.menu[2].show == true) {
+              $location.path('/profile')
+          }
+        });
     }
 
     function getCampaignData() {
@@ -54,18 +61,20 @@ angular.module('locationPluginApp')
           return $scope.campaigns = allCampaigns;
         })
         .then(function (campaigns) {
-          $q.when($scope.connection.getAllTactics(campaigns[0].id))
-            .then(function (allTactics) {
-              return $scope.tactics = allTactics.tactics;
-            })
-            .then(function (tactics) {
-              angular.forEach(tactics, function(tactic, key) {
-                $q.when($scope.connection.getMetricsForTactic(tactic.id))
-                  .then(function (metrics) {
-                    angular.extend($scope.tactics[key], metrics);
-                  })
+          if (campaigns[0]) {
+            $q.when($scope.connection.getAllTactics(campaigns[0].id))
+              .then(function (allTactics) {
+                  return $scope.tactics = allTactics.tactics;
               })
-            });
+              .then(function (tactics) {
+                  angular.forEach(tactics, function(tactic, key) {
+                      $q.when($scope.connection.getMetricsForTactic(tactic.id))
+                          .then(function (metrics) {
+                              angular.extend($scope.tactics[key], metrics);
+                          })
+                  })
+              });
+          }
         })
         .catch(function(response) {
           if (response.status == 0) {
