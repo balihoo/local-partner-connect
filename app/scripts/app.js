eaed62ecf6ff9724715060efb3349f8926f9fcc7
Jordan Lutz <jlutz@balihoo.com>
2016-08-09 10:42:32 -0600

    Update endpoints for prod

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -121,7 +121,7 @@ angular
       this.config = {};
       this.clientId = clientId;
       this.clientApiKey = clientApiKey;
-      this.config.baseUrl = 'https://bac.stage.balihoo-cloud.com';  // Stage environment LPC API
+      this.config.baseUrl = 'https://bac.balihoo-cloud.com';  // Prod environment LPC API
       this.connection = new balihoo.LocalConnection(clientId, clientApiKey, this.config);
     };
     this.destroy = function () {
--- app/scripts/libraries/clientAuth.php
+++ app/scripts/libraries/clientAuth.php
@@ -2,7 +2,7 @@
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Headers: X-Requested-With');
 
-$url = 'https://bac.stage.balihoo-cloud.com/localdata/v1.0/genClientAPIKey';
+$url = 'https://bac.balihoo-cloud.com/localdata/v1.0/genClientAPIKey';
 
 $ch = curl_init();
 
